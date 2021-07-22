5b0840268ee102f5e3c5d3a3c804ed89f6feec61
ArIonMark1 <andreydatsenko777@gmail.com>
2021-07-22 22:23:56 +0200

    lesson2 frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <authors/>
+  
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Authors from './components/Authors.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Authors
   }
 }
 </script>
