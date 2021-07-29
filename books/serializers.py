ce0f83140b8040e9eaeef9823134bfb2d151cd8b
ArIonMark1 <andreydatsenko777@gmail.com>
2021-07-29 14:20:20 +0200

    lesson3 relations

--- books/serializers.py
+++ books/serializers.py
@@ -1,8 +1,14 @@
+from rest_framework.relations import StringRelatedField
+
+from authors.serializers import AuthorSerializer
 from books.models import Book
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.serializers import HyperlinkedModelSerializer, ModelSerializer
 
 
 class BooksSerializer(HyperlinkedModelSerializer):
+    # author = AuthorSerializer(many=True)
+    # author = StringRelatedField()
+
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = 'title', 'description', 'release_year', 'author',
--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,9 @@
-from  rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.serializers import HyperlinkedModelSerializer, ModelSerializer
 from users.models import BaseUser
 
 
 class BaseUserSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = BaseUser
-        fields = 'first_name', 'last_name', 'username', 'email', 'password', 'is_staff',  'is_superuser', 'last_login'
+        fields = 'first_name', 'last_name', 'username', 'email', \
+                 'password', 'is_staff', 'is_superuser', 'last_login', 'is_author'
